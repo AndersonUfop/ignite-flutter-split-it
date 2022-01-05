fb95650554ddba745d64ed3a5d59e86f086c864b
Anderson Fernandes <andersonfferreira13@hotmail.com>
2022-01-05 16:15:28 -0300

    Start the application

---
+++ lib/modeles/app_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Split.it",
+    );
+  }
+}
