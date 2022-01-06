b7a78a91bd9024020df7fe869f8cc9e0b358f510
Anderson Fernandes <andersonfferreira13@hotmail.com>
2022-01-06 16:38:08 -0300

    Finshing splash screen

--- lib/modeles/app_widget.dart
+++ lib/modeles/app_widget.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:split_it/modeles/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Split.it",
+      home: SplashPage(),
     );
   }
 }
